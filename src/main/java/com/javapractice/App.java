0b6d0c7cde4b3377938268f92e5ed740f729c74a
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-21 09:43:48 +0530

    first commit

---
+++ src/main/java/com/javapractice/App.java
@@ -0,0 +1,18 @@
+package com.javapractice;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG= LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        String message= "Hello World!" ;
+        LOG.debug(message+" Will be Printed On Debug");
+        LOG.info(message+""), message, p0, p1, p2, p3, p4, p5, p6, p7);
+        System.out.println(message);
+    }
+}
